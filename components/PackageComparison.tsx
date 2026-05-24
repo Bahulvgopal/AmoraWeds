87e56ee7e52e9ba605e4349b3c6735c1ebbc7a0b
Bahul V Gopal <bahulvgopal@gmail.com>
2026-05-24 19:43:31 +0530

    table added

--- components/PackageComparison.tsx
+++ components/PackageComparison.tsx
@@ -75,9 +75,9 @@ export default function PackageComparison() {
       </div>
 
       {/* ── Footer note ── */}
-      <p className="cmp-note">
+      {/* <p className="cmp-note">
         <span aria-hidden="true">✦</span> All packages include mobile-responsive design and WhatsApp sharing support.
-      </p>
+      </p> */}
 
       <style>{`
         @import url('https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400&family=Jost:wght@300;400;500&display=swap');
